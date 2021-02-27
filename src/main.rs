97840a30d88433afd5c8638b7263fea14fa60dc4
Nic0w <nico.broquet@gmail.com>
2021-02-27 08:32:51 +0100

    Listening on multicast group.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::net::{ UdpSocket, Ipv4Addr };
+
 fn main() {
     println!("Hello, world!");
+
+    let multicast_addr = Ipv4Addr::new(239, 255, 255, 250);
+    let port: u16 = 1900;
+
+    let bind_addr = format!("{addr}:{port}", addr=multicast_addr, port=port);
+
+    let ssdp = UdpSocket::bind(bind_addr).
+        expect("Failed to bind socket.");
+
+    ssdp.join_multicast_v4(&multicast_addr, &Ipv4Addr::UNSPECIFIED).
+        expect("Failed to join multicast group");
+
+    let mut buffer: [u8; 1024] = [0; 1024];
+
+    let (read, src) = ssdp.recv_from(&mut buffer).expect("failed to read!");
+
+    println!("Read {} bytes from {}.", read, src);
+
 }
